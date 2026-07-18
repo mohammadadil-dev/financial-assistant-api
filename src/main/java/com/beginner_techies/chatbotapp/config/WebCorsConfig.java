5313d625e70b21fdc9c1ae9b7cb2459f9d5d2722
Mohammad Adil <adilnitw0786@gmail.com>
2026-07-18 16:18:56 +0300

    added chat stream controller (#6)
    Co-authored-by: mdadil <mdadil@quaratech.com>

--- src/main/java/com/beginner_techies/chatbotapp/config/WebCorsConfig.java
+++ src/main/java/com/beginner_techies/chatbotapp/config/WebCorsConfig.java
@@ -12,7 +12,8 @@ public class WebCorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/chat").allowedOrigins("http://localhost:3000").allowedMethods("POST");
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST",
+						"OPTIONS");
 			}
 		};
 	}
